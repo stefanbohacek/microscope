3684dd929b24786fd372ae0e303e47ab7b24df84
Stefan Bohacek <stefan@stefanbohacek.com>
2022-10-02 02:50:16 -0400

    Removed debug statements.

--- backend/modules/getRepresentativeDetails.js
+++ backend/modules/getRepresentativeDetails.js
@@ -2,12 +2,9 @@ const fs = require("fs");
 const representatives = JSON.parse(fs.readFileSync("data/representatives.json"));
 
 module.exports = async (username, service) => {
-    console.log('debug:representatives', representatives);
-    console.log('debug:username, service', {username, service});
     let representative = {};
     try {
         for (let i = 0, j = representatives.length; i < j; i++){
-            console.log(`debug:representatives[${i}].social`, representatives[i].social);
             if (representatives[i].social[service] && representatives[i].social[service].trim().toLowerCase() === username.trim().toLowerCase()){
                 representative = representatives[i];
                 break;
